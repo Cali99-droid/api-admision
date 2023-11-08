20b46f4ade7ac599ceae1dbe98c0316af846ba48
carlos <orellano428@gmail.com>
2023-11-08 17:40:46 -0500

    consult api SIGE

--- controllers/VacantController.js
+++ controllers/VacantController.js
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { handleHttpError } from "../utils/handleHttpError.js";
 import { body, matchedData } from "express-validator";
+import axios from "axios";
+import FormData from "form-data";
 
 const prisma = new PrismaClient();
 
@@ -112,4 +114,52 @@ const get = async (req, res) => {
   }
 };
 
-export { store, get };
+const getVacantAvailable = async (req, res) => {
+  const loginUrl = process.env.SIGE_URL_LOGIN;
+  const vacantUrl = process.env.SIGE_URL_VACANT;
+  const user = process.env.SIGE_USER;
+  const password = process.env.SIGE_PASSWORD;
+  const formData = new FormData();
+
+  formData.append("login", user);
+  formData.append("password", password);
+  try {
+    const { id_suc, id_niv, id_gra } = req.body;
+
+    const loginResponse = await axios.post(loginUrl, formData);
+    const { data } = loginResponse;
+    const token = data.result.token;
+
+    const matriculaUrl = `${vacantUrl}?id_anio=8&id_suc=${
+      id_suc || ""
+    }&id_niv=${id_niv || ""}&id_gir=1&id_gra=${id_gra || ""}`;
+    const matriculaResponse = await axios.get(matriculaUrl, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    const resp = matriculaResponse.data.result;
+
+    const resultData = resp.map((v) => {
+      return {
+        giro: v.giro,
+        sucursal: v.sucursal,
+        nivel: v.nivel,
+        nom: v.nom,
+        vacantes: v.vacantes,
+      };
+    });
+    // const token = loginResponse.data.token;
+    res.status(200).json({
+      success: true,
+      data: resultData,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error:
+        "Hubo un error al autenticarse o al obtener los datos de matrícula.",
+    });
+  }
+};
+export { store, get, getVacantAvailable };
--- routes/vacant.js
+++ routes/vacant.js
@@ -1,7 +1,11 @@
 import express from "express";
 
 import { authMiddleware } from "../middleware/session.js";
-import { get, store } from "../controllers/VacantController.js";
+import {
+  get,
+  getVacantAvailable,
+  store,
+} from "../controllers/VacantController.js";
 import { validatorGetFamily } from "../validators/family.js";
 import { validatorVacant } from "../validators/vacant.js";
 
@@ -9,5 +13,6 @@ const router = express.Router();
 
 router.post("/:id", validatorGetFamily, validatorVacant, authMiddleware, store);
 router.get("/:id", validatorGetFamily, authMiddleware, get);
+router.get("/check/available", authMiddleware, getVacantAvailable);
 
 export default router;
