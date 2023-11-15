595e37da97a3add84b4b9ecc90b89365a2ce5e81
carlos <orellano428@gmail.com>
2023-11-14 20:01:47 -0500

    changes

--- routes/secretary.js
+++ routes/secretary.js
@@ -4,7 +4,7 @@ import { sessionSecretaryMiddleware } from "../middleware/session.js";
 import { getFamilies } from "../controllers/SecretaryController.js";
 
 const router = express.Router();
-
+//secret
 router.get("/", sessionSecretaryMiddleware, getFamilies);
 
 export default router;
