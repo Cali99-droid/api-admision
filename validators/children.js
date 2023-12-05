1289036d0b3b4be396ed8ab01d2d3e8fadbdabfa
Carlos Orellano <orellano428@gmail.com>
2023-12-05 16:21:32 -0500

    aprroved

--- validators/children.js
+++ validators/children.js
@@ -11,9 +11,8 @@ export const validatorReport = [
 ];
 
 export const validatorInterview = [
-  body("approved").exists().withMessage("El campo familyId es necesario"),
-  ,
-  body("familyId").exists().withMessage("El campo familyId es necesario"),
+  body("approved").exists().withMessage("El campo approved debe ser un numero"),
+  body("familyId").exists().withMessage("El campo familyId debe ser un numero"),
 
   (req, res, next) => {
     return validateResults(req, res, next);
