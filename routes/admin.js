d59ae05db68d2c0fb36a8c2a282dfd579b52c1c2
Adnner Esperilla <adnnerer@hotmail.com>
2024-01-08 15:49:57 -0500

    swagger documentation user

--- routes/admin.js
+++ routes/admin.js
@@ -14,9 +14,88 @@ import {
 } from "../controllers/AdminController.js";
 import { validatorCreateUserRole,validatorUpdateUserRole } from "../validators/userRole.js";
 const router = express.Router();
-
-router.get("/users", adminMiddleware,getAllUsers);
+/**
+ * Get all users
+ * @openapi
+ * /admin/users:
+ *    get:
+ *      tags:
+ *        - Admin
+ *      summary: "Listar usuarios"
+ *      description: Obtiene todos los usuarios
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Retorna la listas de los usuarios.
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/users'
+ *
+ *        '422':
+ *          description: Error de validacion.
+ */
+router.get("/users",adminMiddleware,getAllUsers);
+/**
+ *
+ * Route create user-role
+ * @openapi
+ * /admin/user-role:
+ *      post:
+ *          tags:
+ *              - Admin
+ *          summary: "Asignar  rol a usuario"
+ *          description: "Esta ruta crear rol-user donde token_boss no es obligatorio"
+ *          security:
+ *            - bearerAuth: []
+ *          requestBody:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/userRole"
+ *          responses:
+ *                  '201':
+ *                      description: User-role se creo de manera correcta
+ *                  '401':
+ *                      description: Error por validación de datos
+ *                  '403':
+ *                      description: No tiene permisos '403'
+ *
+ */
 router.post("/user-role",validatorCreateUserRole,adminMiddleware , createUserRole);
+/**
+ * Route put user-role
+ * @openapi
+ * /admin/user-role/{id}:
+ *      put:
+ *          tags:
+ *              - Admin
+ *          summary: "Actualiza el rol-user"
+ *          description: "Esta ruta es para Actualiza datos el rol-user "
+ *          security:
+ *            - bearerAuth: []
+ *          requestBody:
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: "#/components/schemas/userRole"
+ *          parameters:
+ *          - name: id
+ *            in: path
+ *            description: id de rol-user a la cual se le actualizará su rol
+ *            required: true
+ *          responses:
+ *                  '201':
+ *                      description: los datos user-role fue actualizada correctamente
+ *                  '401':
+ *                      description: Error por validación de datos
+ *                  '403':
+ *                      description: No tiene permisos '403'
+ *
+ */
 router.put("/user-role/:id",validatorUpdateUserRole, adminMiddleware, updateUserRole);
 router.get("/secretary/assignments", adminMiddleware, getSecretaryAssignments);
 router.get(
