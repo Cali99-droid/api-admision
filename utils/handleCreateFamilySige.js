2fc7af3e64a1a35393f49319711ae1d6265f58ee
carlos <orellano428@gmail.com>
2024-01-10 09:40:47 -0500

    formdata

--- utils/handleCreateFamilySige.js
+++ utils/handleCreateFamilySige.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const createFamilySIGE = async (name,token) => {
+import FormData from "form-data";
+export const createFamilySIGE = async (name, token) => {
   try {
     const formData = new FormData();
 
@@ -29,7 +30,7 @@ export const createFamilySIGE = async (name,token) => {
     throw error;
   }
 };
-export const createFamiliarsSIGE = async (id_gpf,data,token) => {
+export const createFamiliarsSIGE = async (id_gpf, data, token) => {
   try {
     const formData = new FormData();
 
@@ -37,32 +38,32 @@ export const createFamiliarsSIGE = async (id_gpf,data,token) => {
     formData.append("id_fam", "");
     formData.append("id_gpf", id_gpf);
     formData.append("id_anio", 8);
-    formData.append("es_padre_madre", true)
+    formData.append("es_padre_madre", true);
     formData.append("est", "A");
     /**El role P : PADRE Y M : MADRE */
-    data.person.role === 'P'
-    ?formData.append("id_par", 2)
-    :formData.append("id_par", 1);
+    data.person.role === "P"
+      ? formData.append("id_par", 2)
+      : formData.append("id_par", 1);
     /**El role P : PADRE Y M : MADRE */
-    data.person.type_doc === 'DNI'
-    ?formData.append("id_tdc", 1)
-    :formData.append("id_tdc", 3);
+    data.person.type_doc === "DNI"
+      ? formData.append("id_tdc", 1)
+      : formData.append("id_tdc", 3);
 
     formData.append("nro_doc", data.person.doc_number);
     formData.append("ubigeo", data.person.ubigeo);
     /**El role P : PADRE Y M : MADRE */
-    data.person.role === 'P'
-    /**Si es PADRE entonces es Genero Masculino y su id en SIGE ES :1  */
-    ?formData.append("id_gen", 1)
-    /**Si es MADRE entonces es Genero Femenino y su id en SIGE ES :0  */
-    :formData.append("id_gen", 0);
+    data.person.role === "P"
+      ? /**Si es PADRE entonces es Genero Masculino y su id en SIGE ES :1  */
+        formData.append("id_gen", 1)
+      : /**Si es MADRE entonces es Genero Femenino y su id en SIGE ES :0  */
+        formData.append("id_gen", 0);
     formData.append("ape_pat", data.person.lastname);
     formData.append("ape_mat", data.person.mLastname);
     formData.append("nom", data.person.name);
     /**Cambio de formato de fecha asegun SIGE  */
     const fecha = new Date(data.person.birthdate);
-    const day = fecha.getDate(); 
-    const month = fecha.getMonth() + 1; 
+    const day = fecha.getDate();
+    const month = fecha.getMonth() + 1;
     const year = fecha.getFullYear();
     formData.append("fec_nac", `${day}/${month}/${year}`);
     /**Si es SOLTERO entonces su ID en SIGE ES :1 Y SI NO 2(CASADO) */
@@ -70,8 +71,11 @@ export const createFamiliarsSIGE = async (id_gpf,data,token) => {
     // ?formData.append("id_eci", 1)
     // :formData.append("id_eci", 2);
     //-----SIEMPRE SOLTERO POR MIENTRAS-----
-    formData.append("id_eci", 1)
-    formData.append("prof", data.person.profession !== null ? data.person.profession : "");
+    formData.append("id_eci", 1);
+    formData.append(
+      "prof",
+      data.person.profession !== null ? data.person.profession : ""
+    );
     formData.append("cel", data.phone);
     formData.append("tlf", "");
     formData.append("corr", data.email !== null ? data.email : "");
@@ -89,16 +93,16 @@ export const createFamiliarsSIGE = async (id_gpf,data,token) => {
     throw error;
   }
 };
-export const createStudentSIGE = async (id_gpf,data,token) => {
+export const createStudentSIGE = async (id_gpf, data, token) => {
   try {
     const formData = new FormData();
     formData.append("cod", "");
     formData.append("id_anio", "");
     formData.append("id_gpf", id_gpf);
     // /**El role P : PADRE Y M : MADRE */
-    data.type_doc === 'DNI'
-    ?formData.append("id_tdc", 1)
-    :formData.append("id_tdc", 3);
+    data.type_doc === "DNI"
+      ? formData.append("id_tdc", 1)
+      : formData.append("id_tdc", 3);
 
     formData.append("nro_doc", data.doc_number);
     formData.append("ubigeo", data.ubigeo !== null ? data.person.ubigeo : "");
@@ -106,17 +110,17 @@ export const createStudentSIGE = async (id_gpf,data,token) => {
     formData.append("ape_mat", data.mLastname);
     formData.append("nom", data.name);
     /**El role P : PADRE Y M : MADRE */
-    data.gender === 'M'
-    /**Si es  Genero Masculino y su id en SIGE ES :1  */
-    ?formData.append("id_gen", 1)
-    /**Si es Genero Femenino y su id en SIGE ES :0  */
-    :formData.append("id_gen", 0);
+    data.gender === "M"
+      ? /**Si es  Genero Masculino y su id en SIGE ES :1  */
+        formData.append("id_gen", 1)
+      : /**Si es Genero Femenino y su id en SIGE ES :0  */
+        formData.append("id_gen", 0);
     /**Alumno SOLTERO entonces su ID en SIGE ES :1  */
-    formData.append("id_eci", 1)
+    formData.append("id_eci", 1);
     /**Cambio de formato de fecha asegun SIGE  */
     const fecha = new Date(data.birthdate);
-    const day = fecha.getDate(); 
-    const month = fecha.getMonth() + 1; 
+    const day = fecha.getDate();
+    const month = fecha.getMonth() + 1;
     const year = fecha.getFullYear();
     formData.append("fec_nac", `${day}/${month}/${year}`);
     formData.append("corr", "");
@@ -134,8 +138,7 @@ export const createStudentSIGE = async (id_gpf,data,token) => {
     formData.append("id_cond:", 2);
     formData.append("usuario:", "");
     formData.append("pass_educando:", "");
-    formData.append("est", "A");     
-
+    formData.append("est", "A");
 
     const urlFamiliars = `http://login.ae.edu.pe:8081/sige-mat/api/alumno/alumnoActualizacion`;
 
