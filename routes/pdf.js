8284dc8e47c72e3a009cc4ad6ea970647d4b3f59
Adnner Esperilla <48699352+Adnner-Esperilla-DEV@users.noreply.github.com>
2025-01-15 13:36:32 -0500

    update

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,14 +1,7 @@
 import express from "express";
-
-import { adminMiddleware, authMiddleware } from "../middleware/session.js";
 import {
   downloadVacancyCertificate,
 } from "../controllers/PdfController.js";
-import {
-  validatorCreateRole,
-  validatorUpdateRole,
-} from "../validators/role.js";
-import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
 const router = express.Router();
 /**
  * Descargar constancia de vacante
@@ -31,35 +24,5 @@ const router = express.Router();
  *          description: Error al generar el PDF.
  */
 router.get("/:id", downloadVacancyCertificate);
-/**
- * Get one rol
- * @openapi
- * /role/{id}:
- *    get:
- *      tags:
- *        - Role
- *      summary: "Listar roles"
- *      description: Obtiene todos los roles
- *      security:
- *        - bearerAuth: []
- *      parameters:
- *          - name: id
- *            in: path
- *            description: id de role al cual se busca
- *            required: true
- *      responses:
- *        '200':
- *          description: Retorna la listas de los roles.
- *          content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/roles'
- *
- *        '422':
- *          description: Error de validacion.
- */
-// router.get("/:id", getRoleById);
 
 export default router;
