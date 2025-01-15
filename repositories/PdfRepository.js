2f72ca7bc64b18450de5d90ccc1a5ed4679de61b
Adnner Esperilla <48699352+Adnner-Esperilla-DEV@users.noreply.github.com>
2025-01-15 13:34:44 -0500

    update

--- repositories/PdfRepository.js
+++ repositories/PdfRepository.js
@@ -3,23 +3,7 @@ import PDFDocument from 'pdfkit';
 import path from 'path';
 import { fileURLToPath } from 'url';
 class PdfRepository {
-  // async generateVacancyCertificate() : Promise<Buffer>{
-  //   const doc = new PDFDocument({
-  //     size: 'A4',
-  //     margins: {
-  //       top: 47,
-  //       bottom: 47,
-  //       left: 47,
-  //       right: 66,
-  //     },
-  //   });
-  //   const buffers: Buffer[] = [];
-  //     doc.on('data', buffers.push.bind(buffers));
-  //     doc.on('end', () => resolve(Buffer.concat(buffers)));
-  //   return prisma.year.findMany();
-  // }
   async generateVacancyCertificate(data) {
-    console.log(data);
     return new Promise((resolve, reject) => {
       const doc = new PDFDocument({
         size: 'A4',
@@ -32,20 +16,16 @@ class PdfRepository {
       });
       const buffers = [];
   
-      // Capture the PDF data in memory
       doc.on('data', (chunk) => buffers.push(chunk));
       doc.on('end', () => resolve(Buffer.concat(buffers)));
       doc.on('error', reject);
   
-      // const imagePath = '../img/constancia-header.png'; 
-      // doc.image(imagePath, { fit: [100, 100], align: 'center' });v
       const __filename = fileURLToPath(import.meta.url);
       const __dirname = path.dirname(__filename);
       const imageHeader = path.join(__dirname, '..', 'img', 'constancia-header.png');
       const imageFirma = path.join(__dirname, '..', 'img', 'constancia-firma.png');
-      // doc.image(imagePath, { fit: [100, 100], align: 'center' });
+
       doc.image(imageHeader, 0, 0, { width: doc.page.width, align: 'center' });
-      // Title
       doc.moveDown(6);
       doc.fillColor('#2e559d').fontSize(16).font('Times-Bold').text(`CONSTANCIA DE VACANTE ${data.year}`, {
         align: 'center',
@@ -74,17 +54,8 @@ class PdfRepository {
         `Mi representada: I.E.P. “ALBERT EINSTEIN”, reservará la VACANTE hasta la fecha ${data.endVacant}, quién al momento de matricular deberá presentar la documentación completa, que consta de:`,
         { align: 'justify' }
       );
-  
-      // List of required documents
       doc.moveDown(1);
       
-      // doc.list([
-      //   'Ficha Única de Matrícula actualizada a la fecha (Impreso del SIAGIE).',
-      //   'Resolución Directoral de Traslado.',
-      //   'Fotografía de la Tarjeta de Vacunación (hasta el 3° Primaria).',
-      //   'Constancia de no adeudo.',
-      //   'Libreta de notas final con todas las notas aprobatorias.',
-      // ],{ ordered: false, underline: false });
       const listItems = [
         'Ficha Única de Matrícula actualizada a la fecha (Impreso del SIAGIE).',
         'Resolución Directoral de Traslado.',
@@ -93,44 +64,34 @@ class PdfRepository {
         'Libreta de notas final con todas las notas aprobatorias.'
       ];
       
-      // Coordenadas donde empezar a escribir la lista
-      let yPosition = 350; // Puedes ajustar la posición Y según lo necesites
+
+      let yPosition = 350; 
       
-      // Establece el estilo de texto
       doc.font('Times-Roman').fontSize(12);
       
-      // Escribe cada ítem con numeración, enumerando manualmente sin puntos
+  
       listItems.forEach((item, index) => {
-        // Escribe la numeración manualmente sin el punto
+  
         doc.text(`${index + 1}. ${item}`, 50, yPosition);
         
-        // Incrementa la posición Y para el siguiente ítem
-        yPosition += 15; // Ajusta el espaciado entre los ítems
+        yPosition += 15; 
       });
-      // Important note
       doc.moveDown(1);
       doc.text(
         'Importante: Al no realizar la matrícula hasta la fecha indicada, ocasionará la PÉRDIDA DE VACANTE, disponiendo mi despacho que dicha VACANTE se le otorgue a otro Padre de Familia que cumpla con los requisitos.',
         { align: 'justify', underline: false }
       );
   
-      // Footer with date and place
       doc.moveDown(2);
       doc.text(`Huaraz, ${data.day} de ${data.month} del ${data.year}.`, { align: 'right' });
-      // doc.image(imageFirma, { width: 250, height: 100, align: 'center' });
+
       const imageWidth = 250;
-      const imageHeight = 100;
-      
-      // Obtener las dimensiones de la página
+      const imageHeight = 100;     
       const pageWidth = doc.page.width;
+      const xPosition = (pageWidth - imageWidth) / 2; 
       
-      // Calcular la posición X para mover la imagen hacia la derecha
-      const xPosition = (pageWidth - imageWidth) / 2; // Aumentar 50 para moverla hacia la derecha
-      
-      // Colocar la imagen con la posición X ajustada
       doc.image(imageFirma, xPosition, 550, { width: imageWidth, height: imageHeight });
       
-      // Finalize the PDF
       doc.end();
     });
   }  
